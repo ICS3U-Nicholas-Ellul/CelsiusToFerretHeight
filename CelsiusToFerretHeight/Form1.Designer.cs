5c972cc9bf84a6ee119dbfe9188b39f71a6b2feb
pirates19 <nicholas.ellul@ocsbstudent.ca>
2015-10-26 14:38:31 -0400

    Finish

---
+++ CelsiusToFerretHeight/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CelsiusToFerretHeight
+{
+    partial class frmConvertCTOF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblCel = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(-3, 19);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(302, 24);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "Celsius to fahrenheit calculator.";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(98, 67);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // lblCel
+            // 
+            this.lblCel.AutoSize = true;
+            this.lblCel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCel.Location = new System.Drawing.Point(128, 51);
+            this.lblCel.Name = "lblCel";
+            this.lblCel.Size = new System.Drawing.Size(47, 13);
+            this.lblCel.TabIndex = 4;
+            this.lblCel.Text = "Celsius";
+            this.lblCel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(111, 106);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(297, 24);
+            this.mnuStrip.TabIndex = 7;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            // 
+            // frmConvertCTOF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 136);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblCel);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "frmConvertCTOF";
+            this.Text = "Coversion";
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblCel;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+    }
+}
+
---
+++ CelsiusToFerretHeight/Form1.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by: Nicholas  Ellul
+ * Created on: 26-Oct-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 4-01
+ * This program converts degreesC to degreesF
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CelsiusToFerretHeight
+{
+    public partial class frmConvertCTOF : Form
+    {
+        public void CelsiusToFahrenheit(int Tc)
+        {
+            //converts celsius to fahrenheit
+
+            //variables
+            double Tf;
+            string TfAsString;
+
+            Tf = Tc * (9.0/5.0) + 32;
+            TfAsString = Convert.ToString(Tf);
+            MessageBox.Show("The temperature is " + TfAsString + " degrees fahrenheit.");
+        }
+        public frmConvertCTOF()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //variables
+            int celsius;
+
+            celsius = Convert.ToInt32(this.txtInput.Text);
+            CelsiusToFahrenheit(celsius);
+    
+
+        }
+    }
+}
